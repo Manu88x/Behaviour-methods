a34cb2d1dfc64f0255926612f196f458562156de
Manu88x <emmanuelokoth112@gmail.com>
2024-10-20 19:21:12 +0300

    code

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Your code here
+class Cat {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      return `${this.name} says meow!`;
+    }
+  }
+  
+  class Dog {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      return `${this.name} says woof!`;
+    }
+  }
+  
+  class Bird {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      if (this.sex === 'male') {
+        return `It's me! ${this.name}, the parrot!`;
+      } else {
+        return `${this.name} says squawk!`;
+      }
+    }
+  }
+  
+  // Example usage
+  const cat = new Cat('Whiskers', 'female');
+  console.log(cat.speak()); // "Whiskers says meow!"
+  
+  const dog = new Dog('Rover', 'male');
+  console.log(dog.speak()); // "Rover says woof!"
+  
+  const maleBird = new Bird('Polly', 'male');
+  console.log(maleBird.speak()); // "It's me! Polly, the parrot!"
+  
+  const femaleBird = new Bird('Lola', 'female');
+  console.log(femaleBird.speak()); // "Lola says squawk!"
+  
